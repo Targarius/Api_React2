33d56e479c44572605cbbc20cd5207d5ae8fe211
Targarius <theshark158@gmail.com>
2021-05-08 19:07:34 +0200

    receiving values

---
+++ src/components/FlagCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FlagCard = (props) => {
+    return (
+        <div>
+            <p>
+                {props.attribut}
+            </p>
+        </div>
+    );
+};
+
+export default FlagCard;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import FlagCard from "../components/FlagCard";
 
 const Main = () => {
+    const [countries, setCountries] = useState({});
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch('https://flagcdn.com/en/codes.json')
+        .then((response) => {
+            return response.json()
+        })
+        .then((result) => {
+            setCountries(c => result);
+            setLoaded(true);
+        })
+    }, []); 
+
+    // for (const [key, value] of Object.entries(countries){}
+    //SWAP OBJECT TO ARRAY, ARRAY MAP => FlagCard
+
     return (
         <div>
-            
+          {loaded ? 
+            ("Loaded")
+           : (<h2 className="loading">Loading</h2>)
+          }
         </div>
     );
 };
