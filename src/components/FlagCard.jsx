46ed0d695981144178e6b5754b31889806f7d295
Targarius <theshark158@gmail.com>
2021-05-10 10:17:58 +0200

    cards and basic design

--- src/components/FlagCard.jsx
+++ src/components/FlagCard.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 const FlagCard = (props) => {
+    let Abv = props.attribut[0];
+    let Name = props.attribut[1];
+    let link = `https://en.wikipedia.org/wiki/${Name}`;
+    let srcAbv = `https://flagcdn.com/w160/${Abv}.png`;
+
     return (
-        <div>
-            <p>
-                {props.attribut}
-            </p>
+        <div className="card">
+            <a href={link} target="_blank" rel="noreferrer">
+                <img
+                    src={srcAbv}
+                    width="160"
+                    height="60"
+                    alt={Name} 
+                />
+            <h2>{Name}</h2>
+           </a>
         </div>
     );
 };
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -11,20 +11,53 @@ const Main = () => {
             return response.json()
         })
         .then((result) => {
-            setCountries(c => result);
+            setCountries(c => Object.entries(result));
             setLoaded(true);
         })
     }, []); 
 
-    // for (const [key, value] of Object.entries(countries){}
-    //SWAP OBJECT TO ARRAY, ARRAY MAP => FlagCard
+    const setOrder = () => {
+        // let countriesCopy = countries;
+        // countriesCopy = countriesCopy.reverse();
+        // setCountries(countriesCopy);
+    }
 
     return (
-        <div>
-          {loaded ? 
-            ("Loaded")
-           : (<h2 className="loading">Loading</h2>)
-          }
+        <div className="full_page">
+            <div className="title">
+                <h1>Flags of the world</h1>
+            </div>
+            <div className="filter_container">
+                <input type="text" className="search_bar"/>
+                <div className="radio_btn_wrapper">
+                    <label htmlFor="Z-A">Reverse Order:</label>
+                    <input type="checkbox" name="Z-A" value="Z-A" onChange={setOrder()}/> 
+                </div>
+                <div className="flag_number_wrapper">
+                    <select name="" id="">
+                        <option value="50">
+                            50
+                        </option>
+                        <option value="100">
+                            100
+                        </option>
+                        <option value="200">
+                            200
+                        </option>
+                        <option value="all">
+                            All
+                        </option>
+                    </select>
+                </div>
+            </div>
+            <div className="flag_card_container">
+                {loaded ? 
+                    (countries.map( el => {
+                        return <FlagCard attribut={el} key={countries.indexOf(el)}/>
+                    }))
+                : (<h2 className="loading">Loading</h2>)
+                }
+            </div>
         </div>
     );
 };
